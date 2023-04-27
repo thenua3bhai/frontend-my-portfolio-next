f6e7286b6f24ddef01d37125ef4adf6f1c987afb
Aditya Singh <adityasingh11156@gmail.com>
2023-04-27 12:57:40 +0530

    change name

--- components/Hero.js
+++ components/Hero.js
@@ -19,18 +19,12 @@ function Hero({ pageInfo }) {
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
       {/* we should use next image tag here but for now just we using simple img tag */}
-      <img
-        src={urlFor(pageInfo.heroImage).url()}
-        alt=""
-        className="relative rounded-full h-32 w-32 mx-auto object-cover"
-      />
+      <img src={urlFor(pageInfo.heroImage).url()} alt="" className="relative rounded-full h-32 w-32 mx-auto object-cover" />
       {/* z-20 to make this div above of page and components to make buttons clickable */}
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-          {pageInfo.role}
-        </h2>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">{pageInfo.role}</h2>
         <h1 className="text-5xl lg:text-6xl font-semibold px-10">
-          <span className="mr-3">text</span>
+          <span className="mr-3">Aditya Singh</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
         <div className="pt-5">
